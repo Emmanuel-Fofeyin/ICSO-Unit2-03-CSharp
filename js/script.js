b1a3a7fc5ffcfb6fb25da58003c1682adc8e0287
Emmanuel Fofeyin <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-03-27 23:00:27 +0000

    added code

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Created by: Emmanuel.Fofeyin-Ngwamunwi
+// Created on: march 2022
+// This file contains the JS functions for index.html
+
+/**
+ * enter in adress
+ */
+function enterAdressClicked() {
+
+  document.getElementById('add-streetNumber').innerHTML = '<p>440 + 0 = ' + (440 + 0) + '</p>'
+  document.getElementById('add-streetName').innerHTML = '<p>Longfields + 0 =  ' + (Longfields + 0) + '</p>'
+  document.getElementById('enter-adress').innerHTML = '<p>440 + Longfields = ' + (440 + Longfields) + '</p>'
+}      
--- main.cs
+++ main.cs
@@ -1,7 +1,31 @@
+// Created by: Emmanuel Fofeyin
+// Created on: March 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        string streetNumber;
+        int name;
+
+        Console.WriteLine("This program gets a user's street number and name.");
+        Console.WriteLine("");
+
+        Console.Write("Enter street number: ");
+        Console.Write("Enter street name: ");
+        Console.Write("Your street adress is: ");
+        streetNumber = Console.ReadLine();
+        Console.Write("Enter your name: ");
+        name = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("Your info is: " + streetNumber + ", name " + name + ".");
+
+        Console.WriteLine("\nDone.");
+    }
 }
