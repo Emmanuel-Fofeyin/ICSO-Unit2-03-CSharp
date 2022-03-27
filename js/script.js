cbf31eec9b91d55c6cb97049a5803888d4b35ba4
Emmanuel Fofeyin <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-03-27 23:28:21 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -8,6 +8,6 @@
 function enterAdressClicked() {
 
   document.getElementById('add-streetNumber').innerHTML = '<p>440 + 0 = ' + (440 + 0) + '</p>'
-  document.getElementById('add-streetName').innerHTML = '<p>Longfields + 0 =  ' + (Longfields + 0) + '</p>'
-  document.getElementById('enter-adress').innerHTML = '<p>440 + Longfields = ' + (440 + Longfields) + '</p>'
+  document.getElementById('add-streetName').innerHTML = '<p>Longfields =  ' + (Longfields) + '</p>'
+  document.getElementById('enter-adress').innerHTML = '<p>440 + Longfields = ' + (440 Longfields) + '</p>'
 }      
